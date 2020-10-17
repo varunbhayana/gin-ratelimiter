d61a471853301928dea323401aa76770b732ac33
user <user@tiladmins-MacBook-Pro.local>
2020-10-17 19:49:36 +0530

    rate limiter if release fails then return error

--- handlers/ratelimiter.go
+++ handlers/ratelimiter.go
@@ -3,17 +3,16 @@ package handler
 import (
 	"encoding/json"
 	"fmt"
-	"net/http"
 	"os"
 	"strconv"
 	"time"
 
-	_redis "github.com/go-redis/redis"
-
 	"github.com/bsm/redislock"
 	"github.com/gin-gonic/gin"
+	_redis "github.com/go-redis/redis"
 	"github.com/varunbhayana/gin-ratelimiter/db"
 	"github.com/varunbhayana/gin-ratelimiter/service"
+	"github.com/varunbhayana/gin-ratelimiter/util/cycle_util"
 )
 
 var MAX_MINUTE int
@@ -31,46 +30,59 @@ type RedisModel struct {
 	Count int64 `json:"Count"`
 }
 
-func RateLimit(c *gin.Context) {
+func RateLimit(c *gin.Context) func(*gin.Context) {
+	return func(c *gin.Context) {
+		cycle_util.DegdCall(
+			1*time.Second,
+			c,
+			func() (int, interface{}) {
+				now := time.Now()
+				userId := c.GetHeader("user-id")
+				client := db.GetRedis()
 
-	now := time.Now()
-	userId := c.GetHeader("user-id")
-	client := db.GetRedis()
+				locker := service.NewRedisLock(client)
 
-	locker := service.NewRedisLock(client)
+				lock, err := locker.Obtain(userId+"_lock", 100*time.Millisecond, &service.Options{
+					Context:       client.Context(),
+					Metadata:      "_lock",
+					RetryStrategy: service.LinearBackoff(60 * time.Millisecond),
+				})
+				if err == redislock.ErrNotObtained {
+					return 408, "Retry"
+				} else if err != nil {
+					return 408, "Retry"
 
-	lock, err := locker.Obtain(userId+"_lock", 100*time.Millisecond, &service.Options{
-		Context:       client.Context(),
-		Metadata:      "_lock",
-		RetryStrategy: service.LinearBackoff(60 * time.Millisecond),
-	})
-	if err == redislock.ErrNotObtained {
-		c.String(408, "Retry")
-		return
-	} else if err != nil {
-		c.String(408, "Retry")
-		return
-	}
-	defer lock.Release()
-	fmt.Println("I have a lock!")
-	value, err := client.Get(userId).Result()
-	if err == nil {
-		var redisValue map[int64]int
-		if err := json.Unmarshal([]byte(value), &redisValue); err != nil {
-			panic(err)
-		}
-		if validate(&redisValue, now) {
-			c.String(http.StatusOK, "ok")
-			setForUser(&redisValue, userId, now, client)
-		} else {
-			// too many request response
-			c.String(429, "too many requests")
-		}
-		return
-	} else {
-		setForUser(&map[int64]int{}, userId, now, client)
-		c.String(200, "ok")
-		return
+				}
+				defer func() {
+					err := lock.Release()
+					if err != nil {
+						if x := recover(); x == nil {
+							panic("retry")
+						}
+
+					}
+				}()
+				fmt.Println("I have a lock!")
+				value, err := client.Get(userId).Result()
+				if err == nil {
+					var redisValue map[int64]int
+					if err := json.Unmarshal([]byte(value), &redisValue); err != nil {
+						panic(err)
+					}
+					if validate(&redisValue, now) {
+						setForUser(&redisValue, userId, now, client)
+						return 200, "ok"
+					} else {
+						// too many request response
+						return 429, "too many requests"
+					}
+				} else {
+					setForUser(&map[int64]int{}, userId, now, client)
+					return 200, "ok"
+
+				}
+			},
+		)
 	}
 
 }
